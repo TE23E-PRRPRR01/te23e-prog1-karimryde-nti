89b20f792024f95c4f1cedd7978843de6e100a97
karye <karye.webb@gmail.com>
2024-08-19 13:21:16 +0200

    Testar Neosis

--- Test/Intro/Program.cs
+++ Test/Intro/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+// Skapa en array med några namn med dotnet-8 notation
+string[] namnLista = ["Kalle", "Pelle", "Nisse", "Olle"];
+
+// Skriv ut alla namn i listan
+foreach (string namnet in namnLista)
+{
+    Console.WriteLine($"Namn: {namnet}");
+}
+
+Console.Write("Hej, vad heter du? ");
+string namn = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write($"Hej {namn}! Hur gammal är du? ");
+int ålder = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+if (ålder < 18)
+{
+    Console.WriteLine("Du är inte myndig.");
+}
+else
+{
+    Console.WriteLine("Du är myndig.");
+}
