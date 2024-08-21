9322fbc225baf0b47451c377865a9a1dbe3d7ee3
karye <karye.webb@gmail.com>
2024-08-21 15:20:37 +0200

    Tränat på nytt projekt och konsol-kommandon

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej världen!");
+Console.WriteLine("Nu mitt första C#-program");
+
+Console.Write("Vad heter du? ");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej " + namn);
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Såg du på OS i sommar?");
+Console.WriteLine("Vilken sport gillar du mest?");
