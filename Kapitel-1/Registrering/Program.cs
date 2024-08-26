f83839fd5da3f91e7657b1807ad8045fba87c19b
Karim Ryde <karim.ryde@ga.ntig.se>
2024-08-26 14:45:21 +0200

    Tränat lite på if-satser

--- Kapitel-1/Registrering/Program.cs
+++ Kapitel-1/Registrering/Program.cs
@@ -7,5 +7,16 @@ Console.Write("Ange förnamn: ");
 string förnamn = Console.ReadLine();
 Console.Write("Ange efternamn: ");
 string efternamn = Console.ReadLine();
-Console.WriteLine("Hej! " + förnamn + " " + efternamn);
+//Console.WriteLine("Hej! " + förnamn + " " + efternamn);
 Console.WriteLine($"Hej! {förnamn} {efternamn}");
+
+// Är det Johnnie som loggar in?
+if (förnamn == "Johnnie")
+{
+    Console.WriteLine("Vad trevligt att du är tillbaka!");
+}
+
+if (efternamn == "Gripe")
+{
+    Console.WriteLine("Vackert efternamn!");
+}
