93292981e3d1fe08240706c14415172b6c05e827
Karim Ryde <karim.ryde@ga.ntig.se>
2024-08-26 14:31:55 +0200

    Tränat på cw, cr och svenska tecken

---
+++ Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,11 @@
+﻿// Program för att registrera förnamn och efternamn...
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Program för att registrera förnamn och efternamn...");
+Console.Write("Ange förnamn: ");
+string förnamn = Console.ReadLine();
+Console.Write("Ange efternamn: ");
+string efternamn = Console.ReadLine();
+Console.WriteLine("Hej! " + förnamn + " " + efternamn);
+Console.WriteLine($"Hej! {förnamn} {efternamn}");
