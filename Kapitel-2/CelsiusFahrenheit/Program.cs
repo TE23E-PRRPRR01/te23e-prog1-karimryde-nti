9bd4e45aa4c8dab4cd549cc0da5a28158e8f2c7d
Karim Ryde <karim.ryde@ga.ntig.se>
2024-09-11 14:58:55 +0200

    Tränat datatyper int och double

---
+++ Kapitel-2/CelsiusFahrenheit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Program för att konvertera temperaturen i Celsius till Fahrenheit
+Console.Clear();
+Console.WriteLine("Program för att konvertera temperaturen i Celsius till Fahrenheit");
+
+// Läser in Temp i Celsius
+Console.Write("Ange temperatur i Celsius: ");
+int tempCelsius = int.Parse(Console.ReadLine());
+
+// Konvertera till Fahrenheit
+int tempFahrenheit = tempCelsius * 9 / 5 + 32;
+
+// Skriver ut resultat
+Console.WriteLine($"{tempCelsius} Celsius är {tempFahrenheit} Fahrenheit");
---
+++ Kapitel-2/FahrenheitCelsius/Program.cs
@@ -0,0 +1,13 @@
+﻿// Program för att konvertera från Fahrenheit till Celsius
+Console.Clear();
+Console.WriteLine("Program för att konvertera från Fahrenheit till Celsius");
+
+// Läser in temperaturen i Fahrenheit
+Console.Write("Ange temperaturen i Fahrenheit: ");
+double tempFahrenheit = double.Parse(Console.ReadLine());
+
+// Konvertera Fa -> Ce
+double tempCelsius = (tempFahrenheit - 32) * 5 / 9;
+
+// Skriva ut resultatet med decimaler
+Console.WriteLine($"{tempFahrenheit} F blir {tempCelsius:00.00} C");
