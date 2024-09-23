b13c57d64b5835a3f3dec06dd7fe3a7c64a4e4eb
Karim Ryde <karim.ryde@ga.ntig.se>
2024-09-23 14:49:12 +0200

    Tränat på while-loop och filhantering

---
+++ Kapitel-4/MenyProgram/Program.cs
@@ -0,0 +1,45 @@
+﻿// Litet program med meny
+Console.Clear();
+Console.WriteLine("Litet program med meny");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+// Variabel för valet
+string val = "";
+
+// Programloop som snurrar tills användaren säger avsluta
+while (true)
+{
+    // Visar en meny och läser användarens val
+    Console.Write("""
+    1. Omvandla en text till versaler
+    2. Omvandla en text till gemener
+    3. Avsluta
+    Välj ett av alternativen ovan: 
+    """);
+    val = Console.ReadLine();
+    
+    // Hantera användarens val
+    if (val == "1")
+    {
+        Console.Write("Skriv in en text: ");
+        string texten = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Texten i versaler blir:{texten}");
+    }
+    else if (val == "2")
+    {
+        Console.Write("Skriv in en text: ");
+        string texten = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Texten i versaler blir:{texten}");
+        
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine("Avslutar...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde inget giltigt alternativ, försök igen!");
+    }
+}
---
+++ Kapitel-4/Texteditor/Program.cs
@@ -0,0 +1,50 @@
+﻿// Litet program för att skriva/läsa textfil
+Console.Clear();
+Console.WriteLine("Litet program för att skriva/läsa textfil");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+// Variabel för valet
+string val = "";
+
+// Programloop som snurrar tills användaren säger avsluta
+while (true)
+{
+    // Visar en meny och läser användarens val
+    Console.Write("""
+    1. Skriv till fil
+    2. Läs från fil
+    3. Avsluta
+    Välj ett av alternativen ovan: 
+    """);
+    val = Console.ReadLine();
+
+    // Hantera användarens val
+    if (val == "1")
+    {
+        Console.Write("Skriv in en text: ");
+        string texten = Console.ReadLine();
+        File.WriteAllText("filnamn.txt", texten);
+    }
+    else if (val == "2")
+    {
+        if (File.Exists("filnamn.txt"))
+        {
+            string texten = File.ReadAllText("filnamn.txt");
+            Console.WriteLine(texten);
+        }
+        else
+        {
+            Console.WriteLine("Filen finns inte!");
+        }
+    }
+    else if (val == "3")
+    {
+        Console.WriteLine("Avslutar...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du valde inget giltigt alternativ, försök igen!");
+    }
+}
