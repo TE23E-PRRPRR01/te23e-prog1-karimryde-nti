ef01465323b43521b73133f77b03cac0a0bea16b
Karim Ryde <karim.ryde@ga.ntig.se>
2024-09-30 15:09:13 +0200

    Tränat på List<> och slump, unicode, emoji

---
+++ Kapitel-4/BlackJack/Program.cs
@@ -0,0 +1,97 @@
+﻿// Spelet BlackJack, även kallat 21:an
+Console.Clear();
+Console.WriteLine("Spelet BlackJack");
+
+// Kortens värde är
+// 2-10 = 2-10
+// Knekt, dam, kung = 10
+// Ess = 1 (eller 11)
+// @todo slump måste efterlikna riktig kortlek, dvs 4x1, 4x2...4xkung
+
+// Variabler
+int summaSpelare = 0;
+int summaDator = 0;
+int kort = 0;
+
+// Dela ut 2 kort till spelaren
+kort = Random.Shared.Next(1, 11); // @todo Knekt, dam & kung?
+summaSpelare += kort;
+kort = Random.Shared.Next(1, 11); // @todo Knekt, dam & kung?
+summaSpelare += kort;
+// Dela ut 2 kort till datorn
+kort = Random.Shared.Next(1, 11); // @todo Knekt, dam & kung?
+summaDator += kort;
+kort = Random.Shared.Next(1, 11); // @todo Knekt, dam & kung?
+summaDator += kort;
+
+// Flera gånger (loop)
+while (true)
+{
+    // Skriv ut summorna
+    Console.WriteLine($"Du har {summaSpelare} poäng");
+    Console.WriteLine($"Datorn har {summaDator} poäng");
+
+    // Stanna eller dra ett kort?
+    Console.Write("Vill du ha ett nytt kort? (j/n) ");
+    string svar = Console.ReadLine().ToLower();
+    if (svar == "n")
+    {
+        // @todo datorn får ta extra kort <= 17
+        while (summaDator < 17)
+        {
+            kort = Random.Shared.Next(1, 11); // @todo Knekt, dam & kung?
+            summaDator += kort;
+        }
+
+        // Vem har vunnit?
+        // Den som har närmast 21 har vunnit
+        if (summaSpelare > summaDator && summaSpelare < 21)
+        {
+            Console.WriteLine("Du har vunnit!");
+        }
+        else
+        {
+            Console.WriteLine("Datorn har vunnit!");
+        }
+        break;
+    }
+
+    // Ta ett extra kort
+    kort = Random.Shared.Next(1, 11); // @todo Knekt, dam & kung?
+    summaSpelare += kort;
+    // Skriv ut kortet
+    Console.WriteLine($"Du fick {kort}");
+
+    // Datorn får också ett nytt kort
+    kort = Random.Shared.Next(1, 11); // @todo Knekt, dam & kung?
+    summaDator += kort;
+    // Skriv ut kortet
+    Console.WriteLine($"Datorn fick {kort}");
+
+    // Vem har vunnit?
+    // Har datorn fått 21 o vunnit?
+    if (summaDator == 21)
+    {
+        Console.WriteLine("Datorn har vunnit!");
+        break;
+    }
+    // Har spelaren fått 21 har hen vunnit
+    if (summaSpelare == 21)
+    {
+        Console.WriteLine("Du har vunnit!");
+        break;
+    }
+
+    // Är spelaren tjock?
+    if (summaSpelare > 21)
+    {
+        Console.WriteLine("Du har förlorat!");
+        break;
+    }
+    // Är datorn tjock?
+    if (summaDator > 21)
+    {
+        Console.WriteLine("Du har vunnit!");
+        break;
+    }
+}
---
+++ Kapitel-5/Kortspel/Program.cs
@@ -0,0 +1,32 @@
+﻿// Hur man skapar och använder listor
+Console.Clear();
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Slumpa kort ur ett kortlek!");
+
+// Ett annat exempel. lista på 3 årtal
+//List<int> årtal = [1957, 1969, 1989];
+
+// Skapa en lista kort 
+//List<string> kortlek = ["Ess", "Tvåa", "Trea", "Fyra", "Femma", "Sexa", "Sjua", "Åtta", "Nia", "Tia", "Knekt", "Dam", "Kung"];
+//List<string> kortlek = ["🂡", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮"];
+List<string> kortlek = ["😃", "😘", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮"];
+
+int antal = 5;
+while (antal > 0)
+{
+    // Slumpa index 0-12
+    int index = Random.Shared.Next(0, kortlek.Count);
+    //Console.WriteLine($"Det slumpade index = {index}");
+
+    // Plocka ut det sjunde kortet, index = 6
+    string kort = kortlek[index];
+
+    // Ta bort kortet ur kortleken
+    kortlek.RemoveAt(index);
+
+    // Skriv ut 7:e kortet
+    Console.WriteLine($"Det slumpade kortet är {kort}");
+
+    // Räkna ned
+    antal--;
+}
