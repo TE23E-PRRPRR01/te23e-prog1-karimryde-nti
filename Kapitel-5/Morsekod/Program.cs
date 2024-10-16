15b5f1a0b645b263928a8410d6eb00da3068f433
Karim Ryde <karim.ryde@ga.ntig.se>
2024-10-16 14:59:50 +0200

    Tränat på List<>, index, foreach-loop

---
+++ Kapitel-5/Morsekod/Program.cs
@@ -0,0 +1,62 @@
+﻿// En liten morsekod app 
+Console.Clear();
+Console.WriteLine("En liten morsekod app");
+Console.InputEncoding = System.Text.Encoding.Unicode; 
+
+// Två listor
+List<string> alfabetet = [  "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                            "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                            "S", "T", "U", "V", "W", "X", "Y", "Z", "Å",
+                            "Ä", "Ö", " ", "1", "2", "3", "4", "5",
+                            "6", "7", "8", "9", "0"];
+List<string> morsekod = [ ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+                "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+                "-.--", "--..", ".--.-", ".-.-", "---.", "/" , ".----", 
+                "..---", "...--", "....-", ".....", "-....", "--...", "---..",
+                "----.", "-----"];
+
+// Läs in en meddelande
+Console.Write("Ange en meddelande: ");
+string meddelande = Console.ReadLine();
+
+// Gå igenom meddelandet bokstav för bokstav (loop)
+foreach (char bokstav in meddelande)
+{
+    // Uppslag i alfabetet efter index
+    int index = alfabetet.IndexOf(bokstav.ToString());
+
+    // Hittar morsetecken (A-Ö)?
+    if (index >= 0)
+    {
+        //Console.WriteLine($"{bokstav} finns på index {index}");
+
+        // Plocka ut morsetecknet för detta index
+        string morsetecken = morsekod[index];
+        //Console.WriteLine($"{bokstav} är {morsetecken}");
+        Console.Write($"{morsetecken} ");
+
+        // Spela upp morse som ljud-beep
+        // Tex D = "-.." 
+        // Dvs loopa igenom morsetecknet
+        foreach (char signal in morsetecken)
+        {
+            if (signal == '.') // '.'
+            {
+                // 100Hz, 200ms
+                Console.Beep(1000, 100);
+            }
+            else // '-'
+            {
+                Console.Beep(1000, 300);
+            }
+        }
+
+        // Paus i koden 
+        Thread.Sleep(100);
+    }
+    else
+    {
+        Console.WriteLine("?");
+    }
+}
---
+++ Kapitel-5/Namnregister/Program.cs
@@ -0,0 +1,40 @@
+﻿// Ett litet namnregister
+using System.ComponentModel;
+
+Console.Clear();
+Console.WriteLine("Ett litet namnregister");
+
+List<string> namnlista = [];
+
+// Programloopen
+while (true)
+{
+    // Visa en meny
+    Console.Write("""
+    
+1. Registrera namn
+2. Skriva ut alla namn
+3. Avsluta
+Ange ditt val: 
+""");
+    string val = Console.ReadLine();
+
+    if (val == "1")
+    {
+        Console.Write("Ange ett namn: ");
+        string namn = Console.ReadLine();
+        namnlista.Add(namn);
+    }
+    else if (val == "2")
+    {
+        foreach (var namnet in namnlista)
+        {
+            Console.Write($"{namnet}, ");
+        }
+        Console.WriteLine();
+    }
+    else if (val == "3")
+    {
+        break;
+    }
+}
