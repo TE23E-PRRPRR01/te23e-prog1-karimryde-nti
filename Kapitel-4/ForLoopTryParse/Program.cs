77611c791486bb340f7b820859e75a653c3ea6c9
Karim Ryde <karim.ryde@ga.ntig.se>
2024-11-18 15:17:10 +0100

    Testar wpf

---
+++ Kapitel-4/ForLoopTryParse/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+// Skapa en loop som körs 10 gånger.
+// Varje gång loopen körs ska den skriva ut ditt namn
+
---
+++ Test/MyWpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyWpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnSparaClick(object sender, RoutedEventArgs e)
+    {
+        string förnamn = tbFörnamn.Text;
+        string efternamn = tbEfternamn.Text;
+        MessageBox.Show($"Hej {förnamn} {efternamn}");
+    }
+
+    private void btnHämtaClick(object sender, RoutedEventArgs e)
+    {
+        tbFörnamn.Text = "Kalle";
+        tbEfternamn.Text = "Anka";
+    }
+}
