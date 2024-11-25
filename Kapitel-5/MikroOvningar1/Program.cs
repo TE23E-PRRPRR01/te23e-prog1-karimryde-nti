a9f93342a4b6fbd9cd3c8b48c197c4e2e16a3e7a
Karim Ryde <karim.ryde@ga.ntig.se>
2024-11-25 16:23:54 +0100

    Fix

---
+++ Kapitel-5/MikroOvningar1/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Samlingar");
+
+// Skapa en tom lista av strängar
+List<string> listaBöcker = [];
+
+// Skapa en lista med filmnamn
+// List<string> listaFilmer= ["2001", "I Robot", "Scarface"];
+
+// Skriv ut alla filmnamn
+// Console.WriteLine($"Alla filmer: {string.Join(", ", listaFilmer)}");
+
+// Lägga till i en lista
+listaBöcker.Add("Pippi");
+Console.WriteLine($"Alla böcker: {string.Join(", ", listaBöcker)}");
+// listaFilmer.Add("Black Which");
+// Console.WriteLine($"Alla filmer: {string.Join(", ", listaFilmer)}");
+
+// Användaren anger en bok
+Console.Write("Ange en bok att lägga till: ");
+string nyBok = Console.ReadLine();
+listaBöcker.Add(nyBok);
+Console.WriteLine($"Alla böcker: {string.Join(", ", listaBöcker)}");
+
+// Kolla om en bok finns i listan?
+// Om den finns ta bort den
+Console.Write("Vilken bok söker du? ");
+nyBok = Console.ReadLine();
+
+if (listaBöcker.Contains(nyBok))
+{
+    Console.WriteLine("Den finns");
+    listaBöcker.Remove(nyBok);
+}
+else
+{
+    Console.WriteLine("Den finns inte");
+}
+Console.WriteLine($"Alla böcker: {string.Join(", ", listaBöcker)}");
