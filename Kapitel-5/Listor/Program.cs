83aeb389b491bbfa2c0383c1ef0d624a4c9d500e
Karim Ryde <karye.webb@gmail.com>
2024-12-16 14:58:10 +0100

    Snabb recap

---
+++ Kapitel-5/Listor/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Allt om listor");
+
+// Skapa en tom lista
+List<string> listaNamn = [];
+
+// Infoga i en lista
+listaNamn.Add("Alex");  // Verb
+listaNamn.Add("Alexander");
+Console.WriteLine($"Antal: {listaNamn.Count}");
+
+// Skriv ut listan (går inte)
+Console.WriteLine($"Lista: {listaNamn}");
+
+// Skriv ut lista mha loop
+foreach (var namn in listaNamn)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+// Skriv ut första elementet i listan (index = 0)
+Console.WriteLine($"Första elementet: {listaNamn[0]}");
+
+// Skriv ut två namn ut listan
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"{i}: {listaNamn[i]}");
+}
+
+// Skriv ut alla namn i listan med for-loop
+for (int i = 0; i < listaNamn.Count; i++)
+{
+    Console.WriteLine($"{i}: {listaNamn[i]}");
+}
